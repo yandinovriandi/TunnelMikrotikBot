630da5c5ee63a6fb9fdd85299fa6e052a3ab7bbe
yandi <yandi@yandis-iMac.local>
2022-10-10 01:26:40 +0700

    Edit VerifyCsrf Callback

--- app/Http/Middleware/VerifyCsrfToken.php
+++ app/Http/Middleware/VerifyCsrfToken.php
@@ -12,6 +12,6 @@ class VerifyCsrfToken extends Middleware
      * @var array<int, string>
      */
     protected $except = [
-        'http://tunnel.mikrotikbot.com/confirm-payment'
+        'https://tunnel.mikrotikbot.com/confirm-payment'
     ];
 }
